0a3f76ed8ede6715a24463ccc074660d34eb2fd0
VictorMoorlag <v.moorlag@gmail.com>
2025-01-31 00:26:35 +0100

    Add files via upload

---
+++ PWSSudoku-main/SudokuWpf/Converters/ValueToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SudokuWpf.Converters;
+internal class ValueToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is int intValue)
+        {
+            return intValue == 0 ? Visibility.Hidden : Visibility.Visible;
+        }
+        return Visibility.Hidden;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PWSSudoku-main/SudokuWpf/Helpers/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuWpf.Helpers;
+public static class Extensions
+{
+    public static string ToRowLetter(this int number)
+    {
+        if (number < 0 || number > 8)
+            throw new ArgumentOutOfRangeException(nameof(number), "Number must be between 0 and 8.");
+
+        return ((char)('A' + number)).ToString();
+    }
+
+    public static string ToColumnLetter(this int number)
+    {
+        if (number < 0 || number > 8)
+            throw new ArgumentOutOfRangeException(nameof(number), "Number must be between 0 and 8.");
+
+        return ((char)('a' + number)).ToString();
+    }
+}
